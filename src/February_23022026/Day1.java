f7e6f2706e5931029e035a2528d3c007387b3a1e
maity-arpita <arpita.maity@tuteck.com>
2026-02-24 22:54:48 +0530

    test

---
+++ src/February_23022026/Day1.java
@@ -0,0 +1,7 @@
+package February_23022026;
+
+public class Day1 {
+    public static void main(String[] args){
+        System.out.println("welcome to india!");
+    }
+}
---
+++ src/February_24022026_Main_method/Day2.java
@@ -0,0 +1,7 @@
+package February_24022026_Main_method;
+
+public class Day2 {
+   public static void main(String[] args) {
+       System.out.println("Hi");
+    }
+}
