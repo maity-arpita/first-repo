4bee2f87b26b86672a2556aa9c8314c9420cf8a8
maity-arpita <arpita.maity@tuteck.com>
2026-02-24 23:25:34 +0530

    Save current changes before merging main

--- src/February_23022026/Day1.java
+++ src/February_23022026/Day1.java
@@ -1,7 +1,8 @@
 package February_23022026;
 
 public class Day1 {
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
         System.out.println("welcome to india!");
     }
 }
