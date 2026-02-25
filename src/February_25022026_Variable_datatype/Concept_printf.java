e450daed996f61e52b8c74532399c85bb8793f58
maity-arpita <arpita.maity@tuteck.com>
2026-02-26 00:07:27 +0530

    printf concept done

---
+++ src/February_25022026_Variable_datatype/Concept_printf.java
@@ -0,0 +1,9 @@
+package February_25022026_Variable_datatype;
+
+public class Concept_printf {
+    public static void main(String[] args){
+        int table= 2;
+        System.out.printf("%d x %d = %d",table,5, table*5);
+    }
+
+}
